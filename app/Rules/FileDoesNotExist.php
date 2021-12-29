18da4199cd2150c5e005f167fe87073a3e5c269d
Simon Weckler <simon.weckler@mnet-online.de>
2021-12-29 11:22:29 +0100

    feat: added validation so that no crucial files can be overwritten

---
+++ app/Rules/FileDoesNotExist.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use App\Http\Controllers\SiteController;
+
+class FileDoesNotExist implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $path = SiteController::getFilename($value);
+        error_log("path $path");
+        return !file_exists($path);
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'This file already exists and cannot be overwritten!';
+    }
+}
